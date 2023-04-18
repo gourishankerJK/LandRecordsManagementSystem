3a3b461b7f997a6d0366cec55550a10f1bdf401f
Parvesh Barak <pbarak6@gmail.com>
2023-04-18 19:04:18 +0530

    Profile Form

--- LandRecordsManagement/src/components/common/Input.tsx
+++ LandRecordsManagement/src/components/common/Input.tsx
@@ -1,21 +1,46 @@
-import React from 'react';
-
+import React from 'react'
 
 interface InputProps {
-    label: string;
-    value: string;
-    id: string;
-    onChange: (value: any) => void;
-    [x: string]: any; 
-  }
+  label: string
+  value: string
+  id: string
+  onChange: (value: any) => void
+  [x: string]: any
+}
 
-const Input: React.FC<InputProps> = ({ id , label, value, onChange , ...rest }) => {
+const Input: React.FC<InputProps> = ({
+  id,
+  label,
+  value,
+  onChange,
+  ...rest
+}) => {
   return (
-    <div>
-      <label htmlFor={id}>{label}</label>
-      <input {...rest} onChange={onChange} />
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <label
+        htmlFor={id}
+        style={{
+          padding: '0.5rem 0',
+          fontSize: '15px',
+          letterSpacing: '0.02em',
+        }}
+      >
+        {label}
+      </label>
+      <input
+        {...rest}
+        onChange={onChange}
+        style={{
+          padding: '0.5rem',
+          fontSize: '15px',
+          letterSpacing: '0.02em',
+          border: '1px solid #FF8533',
+          outline: '#FF8533',
+          borderRadius:"5px"
+        }}
+      />
     </div>
-  );
-};
- 
-export default Input;
+  )
+}
+
+export default Input
--- LandRecordsManagement/src/pages/Profile/Profile.tsx
+++ LandRecordsManagement/src/pages/Profile/Profile.tsx
@@ -1,28 +1,87 @@
-import React from 'react';
-import './style.scss';
-import { Verified } from '../../assets';
+import React, { useState } from 'react'
+import './style.scss'
+import { Bg, Verified } from '../../assets'
+import Input from '../../components/common/Input'
 
 interface ProfilePageProps {
-  name: string;
-  photo: string;
-  verified: boolean;
-  aadharNumber: string;
+  name: string
+  photo: string
+  verified: boolean
+  aadharNumber: string
 }
 
-const Profile: React.FC<ProfilePageProps> = ({ name, photo, verified, aadharNumber }) => {
+const Profile: React.FC<ProfilePageProps> = ({
+  name,
+  photo,
+  verified,
+  aadharNumber,
+}) => {
+  const [profile, setProfile] = useState(false)
+  let img = 'sff'
+  const handleInputChange = () => {}
   return (
+    <>
+      {!profile ? (
+        <div className='profile-form'>
+          <h3 className='profile-heading'>Complete Your Profile</h3>
+          <div className='container'>
+            <div className='form-ele'>
+              <div className='profile-img'>
+                {!img ? (
+                  <span className='avatar'>
+                    <p className='initials'>AB</p>
+                  </span>
+                ) : (
+                  <img src={Bg} alt='Profile Img' height='150' />
+                )}
+              </div>
+              <Input
+                id='Images'
+                type='file'
+                onChange={handleInputChange}
+                accept='image/*'
+                label={'Images'}
+                value={''}
+              />
+            </div>
+            <div className='row'>
+              <div className='col'>
+                <Input
+                  id='name'
+                  label='Full Name'
+                  name='name'
+                  value={''}
+                  onChange={handleInputChange}
+                 
+                />
+              </div>
+              <div className='col'>
+                <Input
+                  id='dob'
+                  label='Date Of Birth:'
+                  name='dob'
+                  type='date'
+                  value={'formData.location.district'}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className='profile-page'>
+          <div className='profile-header'>
+            <img src={photo} alt={name} />
+            <h1>{name}</h1>
+            {verified && <span className='verified-badge'>Verified</span>}
+          </div>
+          <div className='aadhar-info'>
+            <p>Aadhar number: {aadharNumber}</p>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
 
-    <div className="profile-page">
-      <div className="profile-header">
-        <img src={photo} alt={name} />
-        <h1>{name}</h1>
-        {verified && <span className="verified-badge">Verified</span>}
-      </div>
-      <div className="aadhar-info">
-        <p>Aadhar number: {aadharNumber}</p>
-      </div>
-    </div>
-  );
-};
-
-export default Profile;
+export default Profile
