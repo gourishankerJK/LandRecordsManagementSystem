26acbe6428a240a7f621fb3d383bd31ea928566e
Gouri Shanker <shanker.gouri@hotmail.com>
2023-04-20 13:51:01 +0530

    Fixed issues in Admin Dashboard

--- LandRecordsManagement/src/pages/AdminDashboard/AdminDashboard.tsx
+++ LandRecordsManagement/src/pages/AdminDashboard/AdminDashboard.tsx
@@ -1,101 +1,108 @@
-
 import React, { useEffect, useState, useContext } from "react";
 import "./styles.scss";
 import LoginContext from "../../contexts/LoginContext";
 import { addOffical, getAllUsers, checkOffical } from "../../utils/admin";
 
-
-
-
 const AdminDashboard = () => {
-    const [users, setUsers] = useState([]);
-    const [officals, setOfficals] = useState([]);
-    const [newOfficalAddress, setNewOfficialAddress] = useState('');
-    const { userContract, accounts, updateMetaMask, setError } = useContext(LoginContext);
+	const [users, setUsers] = useState([]);
+	const [officals, setOfficals] = useState([]);
+	const [newOfficalAddress, setNewOfficialAddress] = useState("");
+	const { userContract, accounts, updateMetaMask, setError } =
+		useContext(LoginContext);
 
-    const handleAddOfficial = (address) => {
-        if(address)  (async () => await addOffical(userContract, accounts, address))();
-        else (async () => await addOffical(userContract, accounts, newOfficalAddress))();
-    }
-    const handleCheckOfficial = () => {
-        setError("Gouri");
-        (async () => {
-            const data = await checkOffical(userContract, accounts, newOfficalAddress);
-            if (data) alert('Yes');
-            else alert('No');
-        })();
-    }
+	const handleAddOfficial = (address) => {
+		if (address)
+			(async () => await addOffical(userContract, accounts, address))();
+		else
+			(async () =>
+				await addOffical(userContract, accounts, newOfficalAddress))();
+	};
+	const handleCheckOfficial = () => {
+		(async () => {
+			const data = await checkOffical(
+				userContract,
+				accounts,
+				newOfficalAddress
+			);
+			if (data) alert("Yes");
+			else alert("No");
+		})();
+	};
 
-    const handleRemoveOfficial = (address) => {
+	const handleRemoveOfficial = (address) => {};
 
-    }
-
-    const handlePromoteUser = (address) => {
-        handleAddOfficial(address);
-
-    }
-    useEffect(() => {
-
-        (async () => {
-            const data = await getAllUsers(userContract, accounts);
-            console.log(data);
-            if (data) {
-                let temp = [...data[0]];
-                for (let i = 0; i < temp.length; i++) {
-                    temp[i] = { ...temp[i], "isGovt": data[1][i] }
-                }
-                setUsers(temp);
-            }
-        })();
-
-    }, [userContract])
-    console.log(users);
-    return (
-        <div className="admin-dashboard">
-            <div className="add-official">
-                <input
-                    type="text"
-                    placeholder="Enter address of the User"
-                    value={newOfficalAddress}
-                    onChange={(e) => setNewOfficialAddress(e.target.value)}
-                />
-                <button onClick={handleAddOfficial}>Add Official</button>
-            </div>
-            <div className="add-official">
-                <input
-                    type="text"
-                    placeholder="Enter address of the User"
-                    value={newOfficalAddress}
-                    onChange={(e) => setNewOfficialAddress(e.target.value)}
-                />
-                <button onClick={handleCheckOfficial}>Check Official</button>
-            </div>
-            <div className="officials">
-                <h2>Officials</h2>
-                <ul>
-                    {users.map((official) => {
-                        if (official.isGovt)
-                            return (<li key={official.my}>  {official.name}
-                                <button onClick={() => handleRemoveOfficial(official.my)}>Remove</button>
-                            </li>)
-                    })}
-                </ul>
-            </div>
-            <div className="users">
-                <h2> Users</h2>
-                <ul>
-                    {users.map((user) => {
-                        if (!user.isGovt) {
-                            return <li key={user.my}>{user.name}
-                                <button onClick={() => handlePromoteUser(user.my)}>Promote</button>
-                            </li>
-                        }
-
-                    })}
-                </ul>
-            </div>
-        </div>
-    );
+	const handlePromoteUser = (address) => {
+		handleAddOfficial(address);
+	};
+	useEffect(() => {
+		(async () => {
+			const data = await getAllUsers(userContract, accounts);
+			if (data) {
+				let temp = [...data[0]];
+				for (let i = 0; i < temp.length; i++) {
+					temp[i] = { ...temp[i], isGovt: data[1][i] };
+				}
+				setUsers(temp);
+			}
+		})();
+	}, [userContract]);
+	return (
+		<div className="admin-dashboard">
+			<div className="add-official">
+				<input
+					type="text"
+					placeholder="Enter address of the User"
+					value={newOfficalAddress}
+					onChange={(e) => setNewOfficialAddress(e.target.value)}
+				/>
+				<button onClick={handleAddOfficial}>Add Official</button>
+			</div>
+			<div className="add-official">
+				<input
+					type="text"
+					placeholder="Enter address of the User"
+					value={newOfficalAddress}
+					onChange={(e) => setNewOfficialAddress(e.target.value)}
+				/>
+				<button onClick={handleCheckOfficial}>Check Official</button>
+			</div>
+			<div className="officials">
+				<h2>Officials</h2>
+				<ul>
+					{users.map((official) => {
+						if (official.isGovt)
+							return (
+								<li key={official.my}>
+									<span>{official.name}</span>
+									<span>{official.my}</span>
+									<button onClick={() => handleRemoveOfficial(official.my)}>
+										Remove
+									</button>
+								</li>
+							);
+					})}
+				</ul>
+			</div>
+			<div className="users">
+				<h2> Users</h2>
+				<ul>
+					{users.map((user) => {
+						if (!user.isGovt) {
+							return (
+								<li key={user.my}>
+									<span>{user.name}</span>
+									<span>{user.my}</span>
+									<button onClick={() => handlePromoteUser(user.my)}>
+										Promote
+									</button>
+								</li>
+							);
+						}
+					})}
+				</ul>
+			</div>
+		</div>
+	);
 };
 
-export default AdminDashboard;  
+export default AdminDashboard;
--- LandRecordsManagement/src/pages/Dashboard/Dashboard.tsx
+++ LandRecordsManagement/src/pages/Dashboard/Dashboard.tsx
@@ -1,106 +1,119 @@
-import React, { useContext, useEffect } from 'react'
-import './style.scss'
-import { Routes, Route, Link, Navigate } from 'react-router-dom'
-import { Logo, Add, DashboradIcon, NotificationIcon, LogoutIcon, Details, SettingIcon } from '../../assets'
-import { AddRecord, UserDashboard, Profile } from '../index'
-import LandDetails from '../LandDetails/LandDetails'
-import AdminDashboard from '../AdminDashboard/AdminDashboard'
-import DashboardHeader from '../common/DashBoardHeader'
-import LoginContext from '../../contexts/LoginContext'
-import ProfileContext from '../../contexts/ProfileContext'
-import { getProfile } from '../../utils/admin'
-import { getDataAsUrl } from '../../utils/ipfs'
-import GovDashboard from '../GovOfficialDashboard/GovDashboard'
+import React, { useContext, useEffect } from "react";
+import "./style.scss";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+import {
+	Logo,
+	Add,
+	DashboradIcon,
+	NotificationIcon,
+	LogoutIcon,
+	Details,
+	SettingIcon,
+} from "../../assets";
+import { AddRecord, UserDashboard, Profile } from "../index";
+import LandDetails from "../LandDetails/LandDetails";
+import AdminDashboard from "../AdminDashboard/AdminDashboard";
+import DashboardHeader from "../common/DashBoardHeader";
+import LoginContext from "../../contexts/LoginContext";
+import ProfileContext from "../../contexts/ProfileContext";
+import { getProfile } from "../../utils/admin";
+import { getDataAsUrl } from "../../utils/ipfs";
+import GovDashboard from "../GovOfficialDashboard/GovDashboard";
 
 const Dashboard = () => {
-  const {updateMetaMask ,userContract , accounts} = useContext(LoginContext)
-  const { userProfile, updateProfile ,setProfilePhoto } = useContext(ProfileContext);
-  useEffect(() => {
-    updateMetaMask();
-    (async function fetch() {
-      const data = await getProfile(userContract, accounts);
-      if (data) {
-        const temp = await getDataAsUrl(data.profilePhoto, 'image/jpeg');
-        updateProfile(data);
-        setProfilePhoto(temp);
-      }
+	const { updateMetaMask, userContract, accounts } = useContext(LoginContext);
+	const { userProfile, updateProfile, setProfilePhoto } =
+		useContext(ProfileContext);
+	useEffect(() => {
+		(async function fetch() {
+			const data = await getProfile(userContract, accounts);
+			if (data) {
+				const temp = await getDataAsUrl(data.profilePhoto, "image/jpeg");
+				updateProfile(data);
+				setProfilePhoto(temp);
+			}
+		})();
+	}, [userContract]);
 
-    })();
-  }, [])
+	return (
+		<div id="dashboard">
+			{/* Sidebar  */}
+			<div className="sidebar">
+				<div className="logo">
+					<img src={Logo} alt="" />
+					<span>LandRecord</span>
+				</div>
+				<div className="side-menu">
+					<div className="side-menu-top">
+						<ul>
+							<Link
+								style={{ textDecoration: "none" }}
+								to="/dashboard/addrecord"
+							>
+								<li className="side-nav-item add">
+									<img src={Add} alt="" />
+									<span>Add Record</span>
+								</li>
+							</Link>
+							<Link style={{ textDecoration: "none" }} to="/dashboard/user">
+								<li className="side-nav-item">
+									<img src={DashboradIcon} alt="" />
+									<span>Dashboard</span>
+								</li>
+							</Link>
+							<Link style={{ textDecoration: "none" }} to="/dashboard/gov">
+								<li className="side-nav-item">
+									<img src={DashboradIcon} alt="" />
+									<span>Dashboard</span>
+								</li>
+							</Link>
+							<Link
+								style={{ textDecoration: "none" }}
+								to="/dashboard/land-details"
+							>
+								<li className="side-nav-item">
+									<img src={Details} alt="" />
+									<span>Land Details</span>
+								</li>
+							</Link>
+							<Link style={{ textDecoration: "none" }} to="/dashboard/profile">
+								<li className="side-nav-item">
+									<img src={SettingIcon} alt="" />
+									<span>Profile</span>
+								</li>
+							</Link>
+						</ul>
+					</div>
+					<div className="side-menu-bottom">
+						<ul>
+							<li className="side-nav-item">
+								<img src={NotificationIcon} alt="" />
+								<span>Notifications</span>
+							</li>
+							<li className="side-nav-item">
+								<img src={LogoutIcon} alt="" />
+								<span style={{ color: "#FF8533" }}>Logout</span>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div className="main-page">
+				<DashboardHeader value={userProfile}></DashboardHeader>
+				<Routes>
+					<Route path="addrecord" element={<AddRecord />} />
+					<Route path="user" element={<UserDashboard />} />
+					<Route path="profile" element={<Profile />} />
+					<Route path="land-details" element={<LandDetails />} />
+					<Route path="admin" element={<AdminDashboard />} />
+					<Route path="gov" element={<GovDashboard />} />
+					<Route path="/" element={null} />
+					<Route path="*" element={<Navigate to="404" />} />
+				</Routes>
+			</div>
+			{/* Page Content  */}
+		</div>
+	);
+};
 
-  return (
-    <div id='dashboard'>
-      {/* Sidebar  */}
-      <div className="sidebar">
-        <div className="logo">
-          <img src={Logo} alt="" />
-          <span >LandRecord</span>
-        </div>
-        <div className="side-menu">
-          <div className="side-menu-top" >
-            <ul>
-              <Link style={{ textDecoration: "none" }} to='/dashboard/addrecord'>
-                <li className='side-nav-item add'>
-                  <img src={Add} alt="" />
-                  <span>Add Record</span>
-                </li>
-              </Link>
-              <Link style={{ textDecoration: "none" }} to='/dashboard/user'>
-                <li className='side-nav-item'>
-                  <img src={DashboradIcon} alt="" />
-                  <span>Dashboard</span>
-                </li>
-              </Link>
-              <Link style={{ textDecoration: "none" }} to='/dashboard/gov'>
-                <li className='side-nav-item'>
-                  <img src={DashboradIcon} alt="" />
-                  <span>Dashboard</span>
-                </li>
-              </Link>
-              <Link style={{ textDecoration: "none" }} to='/dashboard/land-details'>
-                <li className='side-nav-item'>
-                  <img src={Details} alt="" />
-                  <span>Land Details</span>
-                </li>
-              </Link>
-              <Link style={{ textDecoration: "none" }} to='/dashboard/profile'>
-                <li className='side-nav-item'>
-                  <img src={SettingIcon} alt="" />
-                  <span>Profile</span>
-                </li>
-              </Link>
-            </ul>
-          </div>
-          <div className="side-menu-bottom" >
-            <ul>
-              <li className='side-nav-item'>
-                <img src={NotificationIcon} alt="" />
-                <span>Notifications</span>
-              </li>
-              <li className='side-nav-item'>
-                <img src={LogoutIcon} alt="" />
-                <span style={{ color: "#FF8533" }}>Logout</span>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="main-page">
-        <DashboardHeader value = {userProfile}></DashboardHeader>
-        <Routes>
-          <Route path="addrecord" element={<AddRecord />} />
-          <Route path="user" element={<UserDashboard />} />
-          <Route path="profile" element={<Profile/>} />
-          <Route path="land-details" element={<LandDetails />} />
-          <Route path="admin" element={<AdminDashboard />} />
-          <Route path="gov" element={<GovDashboard />} />
-          <Route path="/" element={null} />
-          <Route path="*" element={<Navigate to='404' />} />
-        </Routes>
-      </div>
-      {/* Page Content  */}
-    </div>
-  )
-}
-
-export default Dashboard
+export default Dashboard;
